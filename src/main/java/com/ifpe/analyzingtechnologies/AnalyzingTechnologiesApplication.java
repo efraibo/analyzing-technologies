ad625d7262d47536873735e9f585e33ab70cf700
efraibo <efraibo@gmail.com>
2020-07-06 19:42:20 -0300

    Adicionando endpoint test

--- src/main/java/com/ifpe/analyzingtechnologies/AnalyzingTechnologiesApplication.java
+++ src/main/java/com/ifpe/analyzingtechnologies/AnalyzingTechnologiesApplication.java
@@ -3,9 +3,11 @@ package com.ifpe.analyzingtechnologies;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Slf4j
 @SpringBootApplication
+@EnableJpaRepositories("com.ifpe.analyzingtechnologies.dao.repository")
 public class AnalyzingTechnologiesApplication {
 
     public static void main(String[] args) {
