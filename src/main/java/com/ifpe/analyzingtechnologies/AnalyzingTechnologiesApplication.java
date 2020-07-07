a79d50fa94bf964e9d99c004b1a62bbd182c195c
Evandro <efraibo@gmail.com>
2020-07-06 22:03:26 -0300

    make it better

--- src/main/java/com/ifpe/analyzingtechnologies/AnalyzingTechnologiesApplication.java
+++ src/main/java/com/ifpe/analyzingtechnologies/AnalyzingTechnologiesApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Slf4j
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class AnalyzingTechnologiesApplication {
 
     public static void main(String[] args) {
