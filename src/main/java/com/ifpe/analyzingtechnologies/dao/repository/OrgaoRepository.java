bab251b81ea7373c89c1d5ec83ad5fd1bed2c0a4
Evandro <efraibo@gmail.com>
2020-07-06 21:59:11 -0300

    make it better

--- src/main/java/com/ifpe/analyzingtechnologies/dao/repository/OrgaoRepository.java
+++ src/main/java/com/ifpe/analyzingtechnologies/dao/repository/OrgaoRepository.java
@@ -2,9 +2,11 @@ package com.ifpe.analyzingtechnologies.dao.repository;
 
 import com.ifpe.analyzingtechnologies.dao.entities.Orgao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OrgaoRepository extends JpaRepository<Orgao, Long> {
     List<Orgao> findByStatusFalse();
 }
