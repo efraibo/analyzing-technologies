b8987457bf1b23268d49b46f08b5cd5792b9f9a0
Evandro <efraibo@gmail.com>
2020-07-12 16:51:28 -0300

    deploy heroku

--- src/main/java/com/ifpe/analyzingtechnologies/AnalyzingTechnologiesApplication.java
+++ src/main/java/com/ifpe/analyzingtechnologies/AnalyzingTechnologiesApplication.java
@@ -3,6 +3,9 @@ package com.ifpe.analyzingtechnologies;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Slf4j
 @SpringBootApplication
@@ -10,6 +13,16 @@ public class AnalyzingTechnologiesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnalyzingTechnologiesApplication.class, args);
+    }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+
+            }
+        };
     }
 
 }
